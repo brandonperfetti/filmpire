aa9b7423853aa0fc114474c1282c2718ef904ce3
Brandon Perfetti <brandon@brandonperfetti.com>
2024-08-15 12:32:15 -0700

    Install ShadCN/UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="flex justify-center h-screen items-center bg-gray-200">
+    <div className="flex flex-col justify-center h-screen items-center bg-gray-200">
       <h1 className="text-3xl font-bold text-blue-800">
         Install & Setup Tailwind CSS + React 18+ Typescript
       </h1>
+      <div className="my-4">
+        <Button onClick={() => setCount(count + 1)}>Count is {count}</Button>
+      </div>
     </div>
   );
 }
